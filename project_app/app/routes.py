7a8c400217b1b887b61cf4961c45ab38d0a150cc
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2026-01-12 13:44:22 +0100

    added simple HTML page

--- project_app/app/routes.py
+++ project_app/app/routes.py
@@ -1,5 +1,6 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def home():
-    return f'Server started in port {app.config["PORT"]}'
+    return render_template('index.html')
