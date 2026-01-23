210ed5d7a254cebf40e92c7bc0c93165caa7fd62
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2026-01-23 15:33:40 +0100

    feat: add config map for log configuration and update deployment to use it

--- log_output/app/routes.py
+++ log_output/app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template
 from app import app
 from collections import deque
@@ -13,11 +14,14 @@ def get_pongs():
         return response.json().get("pings", 0)
     except requests.RequestException:
         return 0
-    
+env_var = os.getenv("MESSAGE")
+
 @app.route('/')
 def home(): 
-    log_file_path = 'files/log_app_logs' 
-    with open(log_file_path, 'r') as f:
+    log_file_path = 'files/log_app_logs'
+    info_file_path = 'config/information.txt'
+    with open(log_file_path, 'r') as f, open(info_file_path, 'r') as info_file:
         content  = deque(f, maxlen=1)[0]  # Keep only the last line
+        info_content = info_file.read()
     counter = get_pongs()
-    return render_template('index.html', contents=content, counter=counter)
+    return render_template('index.html', contents=content, counter=counter, info_content=info_content, env_var=env_var)
