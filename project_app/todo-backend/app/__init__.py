40ceb2c1b0184dffcca314ea3f7fe39f53edcb78
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2026-01-24 20:59:43 +0100

    feat: update todo backend to use PostgreSQL with new database integration and environment variables

--- project_app/todo-backend/app/__init__.py
+++ project_app/todo-backend/app/__init__.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template_string, jsonify, request  
 from flask_cors import CORS
+import os
+import sqlalchemy as sa
 
 app = Flask(__name__) 
 CORS(app)
- 
- 
-list_of_todos = []
+
+database_url = os.getenv("POSTGRES_URL")
+engine = sa.create_engine(database_url)
+metadata = sa.MetaData()
+todos = sa.Table(
+	'todos',
+	metadata,
+	sa.Column("id", sa.Integer, primary_key=True),
+	sa.Column('task', sa.String, nullable=False, unique=True)
+)
+metadata.create_all(engine)
+
+def get_all_todos():
+	with engine.connect() as conn:
+		result = conn.execute(sa.select(todos.c.task)).scalars().all()
+	todo_list = result if result is not None else []
+	return todo_list
+
+def insert_todo(task):
+	with engine.begin() as conn:
+		conn.execute(todos.insert().values(task=task))
     
 @app.route('/todos',methods=['GET', 'POST'])
 def home():
+	list_of_todos = get_all_todos()
 	if request.method == "GET":
 		return render_template_string('''
 			{% for todo in todos %}
@@ -16,10 +37,13 @@ def home():
 			{% endfor %}
 		''', todos=list_of_todos), 200
 	else:
-		if request.form['todo'] in list_of_todos:
-			return "<p>Todo already exists!</p>", 409
-		elif not request.form['todo'].strip():
+		task = request.form.get('todo', '').strip()
+
+		if not task:
 			return "<p>Todo cannot be empty!</p>", 400
-		list_of_todos.append(request.form['todo'])
-		return f"<li>{request.form['todo']}</li>", 200
+		try:
+			insert_todo(task)
+			return f"<li>{task}</li>", 200
+		except sa.exc.IntegrityError:
+			return "<p>Todo already exists!</p>", 409
  
